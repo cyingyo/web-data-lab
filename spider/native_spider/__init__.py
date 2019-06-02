5d33e9ce2b44f88cdb24b0a1b3862f29836a6dca
cying <cyingyo@gmail.com>
2019-06-02 16:38:44 +0800

    网页基本结构搭建 && 爬取完毕所有数据

--- spider/native_spider/__init__.py
+++ spider/native_spider/__init__.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
 __username__ = 'cying'
-__password__ = 'Hk8Thdtnct'
-__host__ = '39.106.55.9:27017'
+__password__ = 'cying'
+__host__ = '127.0.0.1:123456'
 __mongo_url__ = 'mongodb://{}:{}@{}'.format(__username__, __password__, __host__)
 
 __mg__ = MongoClient(__mongo_url__)
